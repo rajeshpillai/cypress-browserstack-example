67da6c7367600af570da74bae66bf0c9cc6c6416
Rajesh Pillai <rajesh.algorisys@ext.idfy.com>
2022-05-05 14:38:39 +0530

    Login test

--- cypress/integration/e2e/e2e.spec.ts
+++ cypress/integration/e2e/e2e.spec.ts
@@ -9,7 +9,24 @@ describe('e2e', function () {
   })
 
   it('Visit BrowserStack Demo Website', () => {
+    cy.intercept('GET', Cypress.config().baseUrl + 'api/products').as('apiCheck')
     cy.visit('/');
+    cy.wait('@apiCheck');
+  })
+
+  it('Click on Sign In link', () => {
+
+    cy.visit('/');
+    cy.get('#signin', { timeout: 30000 }).click();
+  })
+
+  it('Perform Login', function () {
+
+    cy.get('#username', { timeout: 30000 }).should('be.visible').click({ force: true }).type(user.user4.username + '{enter}');
+    cy.get('#password', { timeout: 30000 }).click({ force: true }).type(user.user4.password + '{enter}');
+    cy.get('#login-btn', { timeout: 30000 }).click();
+
+    cy.url().should('include', 'bstackdemo.com/?signin=true') // => true
   })
 
 });
